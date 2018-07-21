de9c1ceb9b6d1218d8857e34e3b723678fe52436
Tao <werewaswas@live.com>
2018-07-21 20:13:14 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,16 +5,15 @@ class Song
   attr_reader :artist
 
   extend Memorable
-
+  extend Findable
+  
   @@songs = []
 
   def initialize
     @@songs << self
   end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+
 
   def artist=(artist)
     @artist = artist
